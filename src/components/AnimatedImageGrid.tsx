adc57134be4d3864d8b49ffca04b6458fd11e0ef
aaravsolankiii <aaravsolanki2005@gmail.com>
2026-04-08 01:06:02 +0530

    Update AnimatedImageGrid.tsx

--- src/components/AnimatedImageGrid.tsx
+++ src/components/AnimatedImageGrid.tsx
@@ -5,32 +5,32 @@ const AnimatedImageGrid: React.FC = () => {
   
   const images = [
     {
-      src: "https://images.unsplash.com/photo-1518621012118-696072aa579a?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/5WjxMbTp/IMG-2843.jpg",
       caption: "You Made My Dreams Come True",
       delay: 0
     },
     {
-      src: "https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/Ps8GVv8x/IMG-2844.jpg",
       caption: "Every Moment With You",
       delay: 0.2
     },
     {
-      src: "https://images.unsplash.com/photo-1518199266791-5375a83190b7?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/bgvTQ06n/IMG-2846.jpg",
       caption: "Forever In My Heart",
       delay: 0.4
     },
     {
-      src: "https://images.unsplash.com/photo-1516589091380-5d8e87df6999?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/V0vkc9XZ/9a6a468c-b345-46df-ae70-6a4348761aa2.jpg",
       caption: "You Are My Everything",
       delay: 0.6
     },
     {
-      src: "https://images.unsplash.com/photo-1518197336700-b6e7d83b5b93?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/33GnHwS/ef144c78-fb40-4141-ac7b-3d0cd1bf0ad5.jpg",
       caption: "Love Beyond Words",
       delay: 0.8
     },
     {
-      src: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07?w=400&h=400&fit=crop",
+      src: "https://i.ibb.co/MxckH4Tg/IMG-2840.jpg",
       caption: "Beautiful Like You",
       delay: 1.0
     }
