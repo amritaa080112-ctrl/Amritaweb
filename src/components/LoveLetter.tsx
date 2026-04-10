4efd97789050465bb8e2a1eafc7b042e28b5c6a9
amritaa080112-ctrl <amritaa080112@gmail.com>
2026-04-10 17:49:15 +0530

    Update LoveLetter.tsx

--- src/components/LoveLetter.tsx
+++ src/components/LoveLetter.tsx
@@ -12,14 +12,6 @@ const LoveLetter = () => {
           </p>
           <p className="letter-paragraph">
             Our relationship started on 12th February 2023—Hug Day. And just like the day, our story also began with a hug. When you saw me, you didn’t think twice… you just came and hugged me. It was sudden, our first hug, so it should have felt awkward, “par mujhe bilkul awkward nahi laga… balki bohot accha feel hua.”
-            
-            In that moment, everything felt right. Then we shared our first kiss—nervous, unexpected, but beautiful. “sab kuch itna fast hua, par dil ko sab sahi laga.”
-
-            But there was one thing missing… you forgot to propose me that day. Still, maybe it was meant to be that way. Because on 15th February, you finally proposed. “shayad der se kehna bhi zaroori tha, taaki ehsaas aur gehra ho sake…” And just like that, our story truly began—not perfectly planned, but perfectly ours. 💫
-
-            I don’t remember all the details of these three years, but as much as I do, I know one thing—we were really happy together in those moments. “jitna bhi yaad hai, uss waqt hum dono sach mein bohot khush the.”
-
-            Yes, we fought a lot… maybe too much. But that’s also a part of our journey. “shayad ye ladai bhi zaroori thi, kyunki ye bhi hamari kahani ka ek hissa hai.”* And I know someday, we’ll look back at all this and laugh, thinking—“hum itni choti choti aur senseless baaton pe kyun ladte the?” 💫
           </p>
           <p className="letter-paragraph">
             Your smile lights up my world in ways I never thought possible. When I look into your eyes, 
